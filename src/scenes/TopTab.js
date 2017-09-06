eb3596c09ed72af9c9ae742466212e524ed4f7ae
luis-gzz <gamesbyluis@gmail.com>
2017-09-05 23:42:58 -0400

    Added the files for the three tabs and a helper class

---
+++ src/scenes/TopTab.js
@@ -0,0 +1,43 @@
+import React, { Component, PropTypes } from 'react';
+import ReactNative, {StyleSheet, Text, View, Image, ScrollView, Dimensions, StatusBar,  AsyncStorage, TouchableHighlight,TouchableWithoutFeedback} from 'react-native';
+
+var heightScreen=Dimensions.get('window').height;
+var widthScreen=Dimensions.get('window').width;
+
+class TopTab extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+
+  }
+
+  componentWillReceiveProps(){
+
+  }
+
+  componentWillMount(){
+
+  }
+
+  render () {
+
+      return (
+          <View style={stylesView.mainContainer}>
+          <Text> This will have the top _ios</Text>
+          </View>
+      );
+  }
+
+}
+
+
+const stylesView = StyleSheet.create({
+  mainContainer: {
+    alignItems:'center'
+  },
+});
+
+
+
+
+module.exports = TopTab;
---
+++ src/scenes/helper/Globals.js
@@ -0,0 +1,5 @@
+module.exports = {
+  WIDTH: 375,
+  HEIGHT: 667,
+  
+};
