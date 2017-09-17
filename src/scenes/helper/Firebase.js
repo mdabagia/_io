83d032e5e39922f246bc82e7c3d045cd02bbf54b
Rahul <rahulajmera016@gmail.com>
2017-09-17 04:03:25 -0400

    Get last

--- src/scenes/helper/Firebase.js
+++ src/scenes/helper/Firebase.js
@@ -18,4 +18,40 @@ class Firebase {
 
 }
 
+  getLast() {
+
+    firebase.database().ref('messages').queryLimited(toLast: 1).on('value', function(snapshot) {
+      var arrOfMessages = snapshotToArray(snapshot);
+      for (i = 0; i < arrOfMessages.length; i++) {
+          //console.log(arrOfMessages);
+          var randomNum = (Math.random() * (9)) + 1;
+          if (randomNum <= 4) {
+            post = arrOfMessages[i];
+            console.log(post);
+            return;
+        }
+      }
+
+  });
+
+
+
+    function snapshotToArray(snapshot) {
+        var returnArr = [];
+
+        snapshot.forEach(function(childSnapshot) {
+        var item = childSnapshot.val();
+        item.key = childSnapshot.key;
+
+        returnArr.push(item);
+      });
+
+      return returnArr;
+      };
+
+
+
+
+  }
+
 module.exports = Firebase;
